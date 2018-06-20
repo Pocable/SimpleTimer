5ccaba711e2b2098e70e80816756b506bbcea1ef
Thomas Roskewich <roskewic@ualberta.ca>
2018-06-19 23:45:10 -0600

    Add project files.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.focusArea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.AllowPromptAsInput = false;
+            this.maskedTextBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.maskedTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 96F);
+            this.maskedTextBox.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Overwrite;
+            this.maskedTextBox.Location = new System.Drawing.Point(12, 12);
+            this.maskedTextBox.Mask = "00:00:00";
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.PromptChar = '0';
+            this.maskedTextBox.ShortcutsEnabled = false;
+            this.maskedTextBox.Size = new System.Drawing.Size(552, 152);
+            this.maskedTextBox.TabIndex = 5;
+            this.maskedTextBox.TabStop = false;
+            this.maskedTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.maskedTextBox.Enter += new System.EventHandler(this.maskedTextBox_Enter);
+            this.maskedTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.focusArea_MouseDown);
+            this.maskedTextBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.focusArea_MouseMove);
+            this.maskedTextBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.maskedTextBox_MouseUp);
+            // 
+            // focusArea
+            // 
+            this.focusArea.AutoSize = true;
+            this.focusArea.Location = new System.Drawing.Point(689, 54);
+            this.focusArea.Name = "focusArea";
+            this.focusArea.Size = new System.Drawing.Size(0, 13);
+            this.focusArea.TabIndex = 6;
+            this.focusArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.focusArea_MouseDown);
+            this.focusArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.focusArea_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(576, 176);
+            this.Controls.Add(this.focusArea);
+            this.Controls.Add(this.maskedTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Timer";
+            this.TopMost = true;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+        private System.Windows.Forms.Label focusArea;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+
+        public int seconds = 0;
+        public SoundPlayer player = new SoundPlayer();
+        public bool mouseDownBox = false;
+        public bool mouseDownForm = false;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = false;
+            player.SoundLocation = "alarm.wav";
+            player.Load();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            maskedTextBox.Text = BuildDisplay();
+
+            if (seconds > 0)
+            {
+                seconds -= 1;
+            }
+            else
+            {
+                maskedTextBox.ForeColor = Color.Red;
+                player.PlayLooping();
+            }
+        }
+
+        private String BuildDisplay()
+        {
+            String display = "";
+            int hours = seconds / 3600;
+            int minutes = (seconds - (hours * 3600)) / 60;
+            int sec = seconds - (hours * 3600) - (minutes * 60);
+            display = $"{hours.ToString().PadLeft(2, '0')}:{minutes.ToString().PadLeft(2, '0')}:{sec.ToString().PadLeft(2, '0')}";
+            return display;
+        }
+
+        private int ParseDisplay()
+        {
+            String tim = maskedTextBox.Text.Replace(' ', '0');
+            String[] data = tim.Split(':');
+            Console.WriteLine(data[2]);
+            if (data[2].Length == 1)
+            {
+                data[2] = data[2] + "0";
+            }
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i] = data[i].TrimStart('0');
+                if (string.IsNullOrEmpty(data[i]))
+                {
+                    data[i] = "0";
+                }
+            }
+
+            int sec = int.Parse(data[2]);
+            sec += int.Parse(data[1]) * 60;
+            sec += int.Parse(data[0]) * 3600;
+            return sec;
+        }
+
+        private void Stop()
+        {
+            timer1.Stop();
+            player.Stop();
+            maskedTextBox.ForeColor = Color.Black;
+        }
+
+        private void Start()
+        {
+            if (!timer1.Enabled)
+            {
+                seconds = ParseDisplay();
+                timer1.Start();
+            }
+        }
+
+        private void timeDisplay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.KeyChar.ToString(), out int isNum);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                Start();
+                focusArea.Focus();
+            }
+            else if (e.KeyChar == Convert.ToChar(Keys.Space))
+            {
+                Stop();
+                focusArea.Focus();
+            }
+        }
+
+        private void maskedTextBox_Enter(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                focusArea.Focus();
+            }
+        }
+
+        private void focusArea_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownBox = true;
+        }
+
+        private void focusArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDownBox)
+            {
+                focusArea.Focus();
+            }
+        }
+
+        private void maskedTextBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDownBox = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownForm = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDownForm = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDownForm)
+            {
+                this.Location = Cursor.Position;
+            }
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you sure you want to close the timer?", "Timer Close", MessageBoxButtons.YesNo);
+            if(dr == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
